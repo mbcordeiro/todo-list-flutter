8fa9108d543795dbe03cffd10f8b1cb78d4ef927
Matheus Cordeiro <matheusdebarroscordeiro@gmail.com>
2020-12-24 16:23:30 -0300

    Tests

--- lib/src/repositories/todo_repository.dart
+++ lib/src/repositories/todo_repository.dart
@@ -1,17 +1,17 @@
+import 'dart:ffi';
+
 import 'package:dio/dio.dart';
 import 'package:todo_list/src/models/todo_model.dart';
 
 class TodoRepository {
-  final dio = Dio();
+  Dio dio;
   final url = 'https://jsonplaceholder.typicode.com/todos';
+
+  TodoRepository([Dio dio]) : dio = dio ?? Dio();
+
   Future<List<TodoModel>> fetchTodos() async {
     final response = await dio.get(url);
     final list = response.data as List;
-    List<TodoModel> todos = [];
-    for (var json in list) {
-      final todo = TodoModel.fromJson(json);
-      todos.add(todo);
-    }
-    return todos;
+    return list.map((json) => TodoModel.fromJson(json)).toList();
   }
 }
